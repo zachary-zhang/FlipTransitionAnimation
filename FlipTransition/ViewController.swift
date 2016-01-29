4b8d809d7bc78240d8a97488498f8277c1fa76b4
zangqilong <zangqilong@icloud.com>
2016-01-29 15:18:09 +0800

    Initial Commit

---
+++ FlipTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlipTransition
+//
+//  Created by 臧其龙 on 16/1/29.
+//  Copyright © 2016年 臧其龙. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
